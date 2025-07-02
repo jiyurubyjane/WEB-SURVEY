9a879b68fc358078bd5487100ef12b247e7f296e
Giulia Puspo Negoro <giupn14@gmail.com>
2025-07-02 09:35:29 +0700

    feat: Implement survey submission and download feature

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Alamat server backend Anda
+        target: 'http://localhost:3001', // Alamat server backend Anda
         changeOrigin: true,
         secure: false, 
         rewrite: (path) => path.replace(/^\/api/, ''),     
